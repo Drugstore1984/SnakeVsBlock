3fbd5373240ae2d99c4ae445fe916af7853612ae
Drugstore1984 <47272821+Drugstore1984@users.noreply.github.com>
2022-09-13 10:28:46 +0300

    Make right physics movment in FixedUpdate.

--- Assets/Scripts/Snake/SnakeMove.cs
+++ Assets/Scripts/Snake/SnakeMove.cs
@@ -18,7 +18,7 @@ public class SnakeMove : MonoBehaviour
         _cmCamera = FindObjectOfType<CinemachineVirtualCamera>();
         _cmCamera.Follow = transform;
     }
-    void Update()
+    private void FixedUpdate()
     {
         GetDirection();
         MoveByMouse();
@@ -40,7 +40,7 @@ public class SnakeMove : MonoBehaviour
     }
     private void MoveByMouse()
     {
-        _rb.velocity = new Vector3(_direction.x * _sideSpeed * Time.deltaTime, 0, _forwardSpeed * Time.deltaTime);
+        _rb.velocity = new Vector3(_direction.x * _sideSpeed, 0, _forwardSpeed);
     }
     public void SnakeStop()
     {
